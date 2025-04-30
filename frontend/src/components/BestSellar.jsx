48639c9c11d74064c8ede2a13857378f346d2eed
Aanchallll18 <kumariaachal002@gmail.com>
2025-04-30 15:01:12 +0530

    commit

--- frontend/src/components/BestSellar.jsx
+++ frontend/src/components/BestSellar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ProductCart from './ProductCart'
 
 const BestSellar = () => {
   return (
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
       <div>
-
+        <ProductCart/>
       </div>
     </div>
   )
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,35 @@
-import {  createContext, useContext, useState } from "react"
+import {  createContext, useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext=createContext();
 
 export const AppContextProvider=({children})=>{
 
+
+    const curreny=import.meta.VITE_CURRENCY;
+
     const navigate=useNavigate();
     const [user,setUser]=useState(true);
     const [isSeller,setIsSeller]=useState(false)
     const [showUserLogin,setShowUserLogin]=useState(false)
+    const [products,setProducts]=useState([])
 
-    const value={ navigate,user,setUser,setIsSeller,isSeller,showUserLogin,setShowUserLogin}
+    const fetchProducts=async()=>{
+        setProducts(dummyProducts)
+    }
+
+    useEffect(()=>{
+        setProducts()
+    },[])
+
+    const value={ 
+        navigate,
+        user,setUser,
+        setIsSeller,isSeller,showUserLogin,setShowUserLogin,
+        setProducts,products,
+        curreny
+    }
     
     return <AppContext.Provider value={value}>
        {children} 
