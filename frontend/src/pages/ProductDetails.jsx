56012bbfbc6a8b1ff2f10d1722e6c1e36a069818
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-02 00:57:00 +0530

    commit

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -108,11 +108,14 @@ const {id}=useParams()
 						<div className="w-20 h-0.5 bg-primary rounded-full mt-2">
 						</div>
 					</div>
+					<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
 					{
 						relatedProducts.filter((product)=>product.inStock).map((product,index)=>(
 							<ProductCart key={index} product={product}/>
 						))
 					}
+					</div>
+					<button className="mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded text-primary hover:bg-primary/10 transition">See more</button>
 				</div>
 			</div>
 		)
