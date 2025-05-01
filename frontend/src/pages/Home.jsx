783de45448c365b86d92bee74a7c177064d46524
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-01 18:48:32 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import MainBanner from '../components/MainBanner'
 import Category from '../components/Category'
 import BestSellar from '../components/BestSellar'
 import Banner from '../components/Banner'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Category/>
       <BestSellar/>
       <Banner/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 }
