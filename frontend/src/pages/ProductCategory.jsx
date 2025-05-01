a0dcebe1bf28d8551f0f2f3a232e18e51fa65cdc
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-01 22:12:38 +0530

    commit

---
+++ frontend/src/pages/ProductCategory.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useAppContext } from '../context/AppContext'
+import { useParams } from 'react-router-dom'
+import { categories } from '../assets/assets'
+
+const ProductCategory = () => {
+    const {products}=useAppContext()
+    const {category}=useParams()
+
+    const searchCategory=categories.find((item)=>item.path.toLowerCase()=== category)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductCategory
