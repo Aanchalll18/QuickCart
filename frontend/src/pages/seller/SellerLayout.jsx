934f78c0526290af02a78255bb7581728ae046b4
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-03 01:05:28 +0530

    commit

--- frontend/src/pages/seller/SellerLayout.jsx
+++ frontend/src/pages/seller/SellerLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
 import { useAppContext } from '../../context/AppContext';
+import { Link, NavLink } from 'react-router-dom';
 
 const SellerLayout = () => {
 
@@ -11,21 +12,26 @@ const SellerLayout = () => {
         { name: "Product List", path: "/seller/product-list", icon: assets.product_list_icon},
         { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
+    const logout=async()=>{
+        setIsSeller(false);
+    }
 
     return (
         <>
-            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-                <a href="/">
-                    <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                </a>
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white ">
+                <Link to="/">
+                    <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
+                </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout}
+                    className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
             </div>
-            <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
-                {sidebarLinks.map((item, index) => (
-                    <a href={item.path} key={index}
+           <div className='flex'>
+           <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                {sidebarLinks.map((item) => (
+                    <NavLink to={item.path}  key={index}
                         className={`flex items-center py-3 px-4 gap-3 
                             ${index === 0 ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-primary text-primary"
                                 : "hover:bg-gray-100/90 border-white text-gray-700"
@@ -34,9 +40,10 @@ const SellerLayout = () => {
                     >
                         {item.icon}
                         <p className="md:block hidden text-center">{item.name}</p>
-                    </a>
+                    </NavLink>
                 ))}
             </div>
+           </div>
         </>
     );
 };
