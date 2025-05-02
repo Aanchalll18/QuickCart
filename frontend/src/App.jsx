8422ac728443d4288f9dce68cab3093031572459
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-02 11:51:18 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './components/Login'
 import AllProduct from './pages/AllProduct'
 import ProductCategory from './pages/ProductCategory'
 import ProductDetails from './pages/ProductDetails'
+import CartPage from './pages/CartPage'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
@@ -26,6 +27,7 @@ const App = () => {
           <Route path='/products' element={<AllProduct/>}/>
           <Route path='/products/:category' element={<ProductCategory/>}/>
           <Route path='/products/:category/:id' element={<ProductDetails/>}/>
+          <Route path='/cart' element={<CartPage/>}/>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
