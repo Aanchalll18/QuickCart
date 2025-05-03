6b7cc3a55c2164db221bea52c1e59fa786aa1be7
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-04 02:28:27 +0530

    commit

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -47,3 +47,37 @@ export const productList=async()=>{
     }
 };
 
+export const productById=async(req,res)=>{
+    try {
+        const {id}=req.body;
+        const product=await Product.findById(id)
+        res.json({
+            success:true,
+            product
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+};
+
+export const changeStock=async(req,res)=>{
+    try {
+        const {id,inStock}=req.body;
+        await Product.findByIdAndUpdate(id,{inStock})
+        res.json({
+            success:true,
+            message:'stock Updated'
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+            success:false,
+            message:error.message
+        })
+    }
+}
