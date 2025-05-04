91c708d46d76e2f9fe7f59928f09144898e824e2
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-04 13:35:16 +0530

    commit

--- frontend/src/pages/seller/AddProduct.jsx
+++ frontend/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { assets, categories } from "../../assets/assets";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const AddProduct = () => {
 	const [files, setFiles] = useState([]);
@@ -10,7 +12,39 @@ const AddProduct = () => {
 	const [offerPrice, setOfferPrice] = useState("");
 
 	const onSubmitHandler = async (e) => {
-		e.preventDefault();
+		try {
+			e.preventDefault();
+
+			const productData={
+				name,
+				description:description.spilt('/n'),
+				category,
+				price,
+				offerPrice
+			}
+			const formData=new FormData();
+			formData.append('productData',JSON.stringify(productData));
+
+			for(let i=0;i<files.length;i++){
+				formData.append('images',files[i])
+			}
+			const {data}=await axios.post('/api/product/add',formData)
+
+			if(data.success){
+				toast.success(data.message);
+				setName('')
+				setDescription('')
+				setCategory('')
+				setPrice('')
+				setOfferPrice('')
+				setFiles([])
+			}else{
+				toast.error(data.message)
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(data.message)
+		}
 	};
 
 	return (
