bead343d64f6b93889af7007f88179f84ccb177c
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-04 10:07:47 +0530

    commit

---
+++ backend/models/ordersModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const orderSchema=new mongoose.Schema({
+    userId:{type:String,required:true,ref:'product'},
+    items:[ {
+            product:{type:String,required:true,ref:'user'},
+            quatity:{type:Number,required:true},
+        } ],
+        amount:{type:Number,required:true},
+        address:{type:String,required:true},
+        status:{type:String,default:'OrdrPlaced'},
+        paymentType:{type:String,required:true},
+        isPaid:{type:Boolean,required:true},
+
+},{timestamps:true})
+
+const Order=mongoose.models.order || mongoose.model('order',orderSchema)
+
+export default Order;
