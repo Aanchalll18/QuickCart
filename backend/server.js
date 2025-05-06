c8ed6e81b85f2d2c46fc68f447033554a18a54bb
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-06 15:40:12 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const port =process.env.PORT || 4000;
 await connectDb()
 await connectcloudinary()
 
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['https://quick-cart-brown-mu.vercel.app/']
 
 app.post('/stripe',express.raw({type:'application/json'}),stripewebhooks)
 
