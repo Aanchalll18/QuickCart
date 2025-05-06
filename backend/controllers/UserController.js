929cb10e6ff9ff48695dac954c1fff4e3c03de39
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-06 20:14:34 +0530

    commit

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -45,7 +45,7 @@ export const register = async (req, res) => {
 		res.cookie("token", token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production",
-			samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+			SameSite: "None",
 			maxAge: 7 * 24 * 60 * 60 * 1000,
 		});
 
