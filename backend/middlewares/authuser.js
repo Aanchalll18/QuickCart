233c5b14b1b592d489b2b5178d744eba766cce9a
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-06 22:23:44 +0530

    commit

--- backend/middlewares/authuser.js
+++ backend/middlewares/authuser.js
@@ -1,58 +1,58 @@
-// import jwt from 'jsonwebtoken'
-// const authUser=async(req,res,next)=>{
-//     // const {token}=req.cookies;
-//     let token = req.cookies.token;
+import jwt from 'jsonwebtoken'
+const authUser=async(req,res,next)=>{
+    // const {token}=req.cookies;
+    let token = req.cookies.token;
 
-//     console.log("auth",token)
-//     if(!token){
-//         return res.json({
-//             success:false,
-//             message:"User not authorized"
-//         })
-//     }
-//     try {
-//         const decode=jwt.verify(token,process.env.JWT_SECRET)
-//         if(decode.id){
-//             req.userId=decode.id;
-//         }else{
-//             return res.json({
-//                 success:false,
-//                 message:"Invalid token"
-//             })
-//         }
-//         next()
-//     } catch (error) {
-//        res.json({
-//         success:false,
-//         message:error.message
-//        }) 
-//     }
-// }
-// export default authUser;
-
-
-
-
-import jwt from 'jsonwebtoken';
-
-const authuser = async (req, res, next) => {
-    const { token } = req.cookies;
-    console.log("auth", token);
-
-    if (!token) {
-        return res.status(401).json({ success: false, message: "Not Authorized, token missing" });
+    console.log("auth",token)
+    if(!token){
+        return res.json({
+            success:false,
+            message:"User not authorized"
+        })
     }
-
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = tokenDecode.id; // Store decoded userId in req
-        next();
+        const decode=jwt.verify(token,process.env.JWT_SECRET)
+        if(decode.id){
+            req.userId=decode.id;
+        }else{
+            return res.json({
+                success:false,
+                message:"Invalid token"
+            })
+        }
+        next()
     } catch (error) {
-        return res.status(401).json({
-            success: false,
-            message: "Invalid or expired token",
-        });
+       res.json({
+        success:false,
+        message:error.message
+       }) 
     }
-};
+}
+export default authUser;
 
-export default authuser;
+
+
+
+// import jwt from 'jsonwebtoken';
+
+// const authuser = async (req, res, next) => {
+//     const { token } = req.cookies;
+//     console.log("auth", token);
+
+//     if (!token) {
+//         return res.status(401).json({ success: false, message: "Not Authorized, token missing" });
+//     }
+
+//     try {
+//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+//         req.userId = tokenDecode.id; // Store decoded userId in req
+//         next();
+//     } catch (error) {
+//         return res.status(401).json({
+//             success: false,
+//             message: "Invalid or expired token",
+//         });
+//     }
+// };
+
+// export default authuser;
