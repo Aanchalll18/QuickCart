b780c5015f4f9e1b3e2ccaf75fd49428ee08dbd5
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-06 21:45:20 +0530

    commit

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,6 +7,6 @@ const userRouter=express.Router();
 userRouter.post('/register',register)
 userRouter.post('/login',login)
 userRouter.get('/isAuth',authUser,isAuth)
-userRouter.get('/logout',logout)
+userRouter.get('/logout',authUser,logout)
 
 export default userRouter;
