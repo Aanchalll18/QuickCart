7b1ee9f7cc033179b486a9da9a15c3729a3dc0dd
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-07 23:54:46 +0530

    commit

--- backend/controllers/SellerControlers.js
+++ backend/controllers/SellerControlers.js
@@ -28,7 +28,7 @@ export const sellerLogin = async (req, res) => {
 
     res.cookie('sellerToken', sellerToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       sameSite:  'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
